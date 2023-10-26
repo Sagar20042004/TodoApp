0cdb783cbc18cc3ffbbc86a170178b97a3fab08f
Lenovo <sagarbheda2004@gmail.com>
2023-10-26 20:00:15 +0530

    Day 3

---
+++ app/src/main/java/com/sagarbheda/todoapp/Activity_create_card.kt
@@ -0,0 +1,11 @@
+package com.sagarbheda.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_create_card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_card)
+    }
+}
---
+++ app/src/main/java/com/sagarbheda/todoapp/Activity_update_task.kt
@@ -0,0 +1,11 @@
+package com.sagarbheda.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_update_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_task)
+    }
+}
